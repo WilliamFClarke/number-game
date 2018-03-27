c1ed627b2123d52c7110f6d00b51d3d95af634ae
WilliamFClarke <will.fclarke@gmail.com>
2018-03-27 22:13:35 +0100

    Add Get Number endpoint

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
+var bodyParser = require("body-parser");
+var testVal = 0
+
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/numbergame/getnumber', (req, res) => {
+
+  res.send(testVal.toString());
+
+});
+
 
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on env port'))

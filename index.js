7d20adf696ba8b58ecc675ab4e0582fe3f6f0763
WilliamFClarke <will.fclarke@gmail.com>
2018-03-27 22:20:19 +0100

    add setnumber

--- index.js
+++ index.js
@@ -14,5 +14,28 @@ app.get('/numbergame/getnumber', (req, res) => {
 
 });
 
+app.post('/numbergame/setnumber',function(req, res){
+  const userName = req.query.user;
+  const userNumber = req.query.number;
+  const expectedNumber = testVal;
+  console.log(req.query)
+  console.log(userNumber)
+  if (userNumber == expectedNumber) {
+    res.status(200);
+    res.end("CORRECT! You have set number: " + userNumber);
+    increaseVal();
+    return
+  } else {
+    res.end("INCORRECT NUMBER! You enterd: " + userNumber + " the expected number is: " + expectedNumber);
+    return
+  };
+});
 
 app.listen(process.env.PORT || 5000, () => console.log('Example app listening on env port'))
+
+function increaseVal() {
+  var tempVal = testVal
+  testVal += 1;
+  console.log('testval increased to: ' + testVal );
+  return tempVal;
+};
